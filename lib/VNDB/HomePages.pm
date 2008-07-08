3029c4af4b5a7bffa78f96fb9b02c37a2b36891a
yorhel <yorhel@1fe2e327-d9db-4752-bcf7-ef0cb4a1748b>
2008-07-08 10:56:32 +0000

    Replaced recent producers with random visual novels on the homepage
    git-svn-id: svn://vndb.org/vndb@58 1fe2e327-d9db-4752-bcf7-ef0cb4a1748b

--- lib/VNDB/HomePages.pm
+++ lib/VNDB/HomePages.pm
@@ -19,7 +19,7 @@ sub HomePage {
     anpost      => $self->DBGetPosts(tid => $an->{id}, num => 1)->[0],
     recentedits => scalar $self->DBGetHist( results => 10, what => 'iid ititle'),
     recentvns   => scalar $self->DBGetHist( results => 10, what => 'iid ititle', edits => 0, type => 'v'),
-    recentps    => scalar $self->DBGetHist( results => 10, what => 'iid ititle', edits => 0, type => 'p'),
+    randomvns   => scalar $self->DBGetVN(   results => 10, order => 'RANDOM()'),
     recentposts => scalar $self->DBGetThreads(results => 10, what => 'lastpost', order => 'tp2.date DESC'),
    # cache this shit when performance is going to be problematic
     upcomingrel => scalar $self->DBGetRelease(results => 10, unreleased => 1),
