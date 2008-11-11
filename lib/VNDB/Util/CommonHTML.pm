70264d121dac0dda32e77732a356007e8fa29846
Yorhel <git@yorhel.nl>
2008-11-11 13:02:27 +0100

    SQL queries will be added to the HTML comment when debugging is enabled

--- lib/VNDB/Util/CommonHTML.pm
+++ lib/VNDB/Util/CommonHTML.pm
@@ -126,9 +126,22 @@ sub _menu {
 
 
 sub htmlFooter {
+  my $self = shift;
     end; # /div maincontent
    end; # /body
   end; # /html
+
+  # write the SQL queries as a HTML comment when debugging is enabled
+  if($self->debug) {
+    lit "\n<!--\n SQL Queries:\n";
+    for (@{$self->{_YAWF}{DB}{queries}}) {
+      my $q = !ref $_->[0] ? $_->[0] :
+      $_->[0][0].(exists $_->[0][1] ? ' | "'.join('", "', @{$_->[0]}[1..$#{$_->[0]}]).'"' : '');
+      $q =~ s/^\s//g;
+      lit sprintf "  [%6.2fms] %s\n", $_->[1]*1000, $q;
+    }
+    lit "-->\n";
+  }
 }
 
 
