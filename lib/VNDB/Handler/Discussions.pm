044c3534a58dfe2aa131073abde028b3fbc6e7bb
Yorhel <git@yorhel.nl>
2008-11-16 12:00:08 +0100

    Users have maintabs, too

--- lib/VNDB/Handler/Discussions.pm
+++ lib/VNDB/Handler/Discussions.pm
@@ -281,7 +281,7 @@ sub tagbrowse {
 
   $self->htmlHeader(title => $title);
 
-  $self->htmlMainTabs($type, $obj, 'disc') if $iid && $type ne 'u';
+  $self->htmlMainTabs($type, $obj, 'disc') if $iid;
   div class => 'mainbox';
    h1 $title;
    p;
