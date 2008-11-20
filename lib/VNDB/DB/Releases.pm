0b884fcdc1293e517f9a3e4daeaa52c5ef1fc08c
Yorhel <git@yorhel.nl>
2008-11-20 15:35:55 +0100

    Added dbReleaseGet()
    Almost exact copy of DBGetRelease() from 1.x

---
+++ lib/VNDB/DB/Releases.pm
@@ -0,0 +1,111 @@
+
+package VNDB::DB::Releases;
+
+use strict;
+use warnings;
+use Exporter 'import';
+
+our @EXPORT = qw|dbReleaseGet|;
+
+
+# Options: id vid rev order unreleased page results what
+# What: changes vn producers platforms media
+sub dbReleaseGet {
+  my($self, %o) = @_;
+  $o{results} ||= 50;
+  $o{page} ||= 1;
+  $o{what} ||= '';
+  $o{order} ||= 'rr.released ASC';
+
+  my %where = (
+    !$o{id} && !$o{rev} ? (
+      'r.hidden = FALSE' => 0 ) : (),
+    $o{id} ? (
+      'r.id = ?' => $o{id} ) : (),
+    $o{rev} ? (
+      'rv.id = ?' => $o{rev} ) : (),
+    $o{vid} ? (
+      'rv.vid = ?' => $o{vid} ) : (),
+    defined $o{unreleased} ? (
+      q|rr.released !s TO_CHAR('today'::timestamp, 'YYYYMMDD')::integer| => $o{unreleased} ? '>' : '<=' ) : (),
+  );
+
+  my @join = (
+    $o{rev} ? 'JOIN releases r ON r.id = rr.rid' : 'JOIN releases r ON rr.id = r.latest',
+    $o{vid} ? 'JOIN releases_vn rv ON rv.rid = rr.id' : (),
+    $o{what} =~ /changes/ ? (
+      'JOIN changes c ON c.id = rr.id',
+      'JOIN users u ON u.id = c.requester'
+    ) : (),
+  );
+
+  my @select = (
+    qw|r.id r.locked r.hidden rr.title rr.original rr.gtin rr.language rr.website rr.released rr.notes rr.minage rr.type|, 'rr.id AS cid',
+    $o{what} =~ /changes/ ? qw|c.added c.requester c.comments r.latest u.username c.rev| : (),
+  );
+
+  my($r, $np) = $self->dbPage(\%o, q|
+    SELECT !s
+      FROM releases_rev rr
+      !s
+      !W
+      ORDER BY !s|,
+    join(', ', @select), join(' ', @join), \%where,  $o{order}
+  );
+
+  if(@$r && $o{what} =~ /(vn|producers|platforms|media)/) {
+    my %r = map {
+      $r->[$_]{producers} = [];
+      $r->[$_]{platforms} = [];
+      $r->[$_]{media} = [];
+      $r->[$_]{vn} = [];
+      ($r->[$_]{cid}, $_)
+    } 0..$#$r;
+
+    if($o{what} =~ /vn/) {
+      push(@{$r->[$r{$_->{rid}}]{vn}}, $_) for (@{$self->dbAll(q|
+        SELECT rv.rid, vr.vid, vr.title, vr.original
+          FROM releases_vn rv
+          JOIN vn v ON v.id = rv.vid
+          JOIN vn_rev vr ON vr.id = v.latest
+          WHERE rv.rid IN(!l)|,
+        [ keys %r ]
+      )});
+    }
+
+    if($o{what} =~ /producers/) {
+      push(@{$r->[$r{$_->{rid}}]{producers}}, $_) for (@{$self->dbAll(q|
+        SELECT rp.rid, p.id, pr.name, pr.original, pr.type
+          FROM releases_producers rp
+          JOIN producers p ON rp.pid = p.id
+          JOIN producers_rev pr ON pr.id = p.latest
+          WHERE rp.rid IN(!l)|,
+        [ keys %r ]
+      )});
+    }
+
+    if($o{what} =~ /platforms/) {
+      push(@{$r->[$r{$_->{rid}}]{platforms}}, $_->{platform}) for (@{$self->dbAll(q|
+        SELECT rid, platform
+          FROM releases_platforms
+          WHERE rid IN(!l)|,
+        [ keys %r ]
+      )});
+    }
+
+    if($o{what} =~ /media/) {
+      ($_->{medium}=~s/\s+//||1)&&push(@{$r->[$r{$_->{rid}}]{media}}, $_) for (@{$self->dbAll(q|
+        SELECT rid, medium, qty
+          FROM releases_media
+          WHERE rid IN(!l)|,
+        [ keys %r ]
+      )});
+    }
+  }
+
+  return wantarray ? ($r, $np) : $r;
+}
+
+
+1;
+
