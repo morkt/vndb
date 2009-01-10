9663376cd572b6f94c83f9a580c25fbd2bdc67f2
Yorhel <git@yorhel.nl>
2009-01-10 10:23:00 +0100

    Convert all existing patch media in the DB to the new patch flag

--- util/updates/update_2.2.sql
+++ util/updates/update_2.2.sql
@@ -2,6 +2,11 @@
 -- custom CSS
 ALTER TABLE users ADD COLUMN customcss text NOT NULL DEFAULT '';
 
+
+
 -- patch flag
 ALTER TABLE releases_rev ADD COLUMN patch BOOLEAN NOT NULL DEFAULT FALSE;
+UPDATE releases_rev SET patch = TRUE
+  WHERE EXISTS(SELECT 1 FROM releases_media rm WHERE rm.rid = id AND rm.medium = 'pa ');
+DELETE FROM releases_media WHERE medium = 'pa ';
 
