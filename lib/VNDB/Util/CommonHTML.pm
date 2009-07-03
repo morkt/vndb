45843fa3b02a69951fe7f4144ccdcbca76609180
Yorhel <git@yorhel.nl>
2009-07-03 21:42:33 +0200

    Make sure the search-tab-thingy stays nicely above the search box
    It didn't always look good in all screen resolutions.

--- lib/VNDB/Util/CommonHTML.pm
+++ lib/VNDB/Util/CommonHTML.pm
@@ -522,14 +522,14 @@ sub htmlHistory {
 sub htmlSearchBox {
   my($self, $sel, $v) = @_;
 
-  p class => 'searchtabs';
-   a href => '/v/all', $sel eq 'v' ? (class => 'sel') : (), 'Visual novels';
-   a href => '/r',     $sel eq 'r' ? (class => 'sel') : (), 'Releases';
-   a href => '/p/all', $sel eq 'p' ? (class => 'sel') : (), 'Producers';
-   a href => '/g',     $sel eq 'g' ? (class => 'sel') : (), 'Tags';
-   a href => '/u/all', $sel eq 'u' ? (class => 'sel') : (), 'Users';
-  end;
   fieldset class => 'search';
+   p class => 'searchtabs';
+    a href => '/v/all', $sel eq 'v' ? (class => 'sel') : (), 'Visual novels';
+    a href => '/r',     $sel eq 'r' ? (class => 'sel') : (), 'Releases';
+    a href => '/p/all', $sel eq 'p' ? (class => 'sel') : (), 'Producers';
+    a href => '/g',     $sel eq 'g' ? (class => 'sel') : (), 'Tags';
+    a href => '/u/all', $sel eq 'u' ? (class => 'sel') : (), 'Users';
+   end;
    input type => 'text', name => 'q', id => 'q', class => 'text', value => $v;
    input type => 'submit', class => 'submit', value => 'Search!';
   end;
