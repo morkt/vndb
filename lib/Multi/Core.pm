64242a632bde81f6af522b528a21a456467c1f61
Yorhel <git@yorhel.nl>
2009-07-17 11:38:24 +0200

    Multi::Core: Handle SIGTERM and SIGINT
    These events will be signalled to Multi's modules as a 'shutdown'
    signal.

--- lib/Multi/Core.pm
+++ lib/Multi/Core.pm
@@ -22,21 +22,12 @@ sub run {
   $db[0] = "$dsn[0]:$dsn[1]($dsn[2]):$dsn[4]";
   POE::Component::Pg->spawn(alias => 'pg', dsn => $db[0], user => $db[1], password => $db[2]);
 
-  # spawn the core session (which only handles logging at this point)
+  # spawn the core session (which handles logging & external signals)
   POE::Session->create(
     package_states => [
-      $p => [qw| _start log pg_error |],
+      $p => [qw| _start log pg_error sig_shutdown shutdown |],
     ],
   );
-
-  # dynamically load and spawn modules
-  for (keys %{$VNDB::M{modules}}) {
-    my($mod, $args) = ($_, $VNDB::M{modules}{$_});
-    next if !$args || ref($args) ne 'HASH';
-    require "Multi/$mod.pm";
-    # I'm surprised the strict pagma isn't complaining about this
-    "Multi::$mod"->spawn(%$args);
-  }
 
   # log warnings
   $SIG{__WARN__} = sub {(local$_=shift)=~s/\r?\n//;$poe_kernel->call(core=>log=>'__WARN__: '.$_)};
@@ -47,9 +38,21 @@ sub run {
 
 sub _start {
   $_[KERNEL]->alias_set('core');
+  $_[KERNEL]->call(core => log => 'Starting Multi '.$VNDB::S{version});
   $_[KERNEL]->post(pg => register => error => 'pg_error');
   $_[KERNEL]->post(pg => 'connect');
-  $_[KERNEL]->call(core => log => 'Starting Multi '.$VNDB::S{version});
+  $_[KERNEL]->sig(INT => 'sig_shutdown');
+  $_[KERNEL]->sig(TERM => 'sig_shutdown');
+  $_[KERNEL]->sig('shutdown', 'shutdown');
+
+  # dynamically load and spawn modules
+  for (keys %{$VNDB::M{modules}}) {
+    my($mod, $args) = ($_, $VNDB::M{modules}{$_});
+    next if !$args || ref($args) ne 'HASH';
+    require "Multi/$mod.pm";
+    # I'm surprised the strict pagma isn't complaining about this
+    "Multi::$mod"->spawn(%$args);
+  }
 }
 
 
@@ -57,7 +60,7 @@ sub log { # level, msg
   (my $p = eval { $_[SENDER][2]{$_[CALLER_STATE]}[0] } || '') =~ s/^Multi:://;
   my $msg = sprintf '%s::%s: %s', $p, $_[CALLER_STATE],
     $_[ARG1] ? sprintf($_[ARG0], @_[ARG1..$#_]) : $_[ARG0];
-    
+
   open(my $F, '>>', $VNDB::M{log_dir}.'/multi.log');
   printf "[%s] %s\n", scalar localtime, $msg;
   close $F;
@@ -69,6 +72,20 @@ sub pg_error { # ARG: command, errmsg, [ query, params, orig_session, event-args
     join(', ', $_[KERNEL]->alias_list($_[ARG4])), $_[ARG2],
     join(', ', $_[ARG3] ? map qq|"$_"|, @{$_[ARG3]} : '[none]'), $_[ARG5] : '';
   $_[KERNEL]->call(core => log => 'SQL Error for command %s: %s %s', $_[ARG0], $_[ARG1], $s);
+}
+
+
+sub sig_shutdown {
+  # Multi modules should listen to the shutdown signal (but should never call sig_handled() on it!)
+  $_[KERNEL]->signal($_[SESSION], 'shutdown', 'SIG'.$_[ARG0]);
+  # consider this event as handled, so our process won't be killed directly
+  $_[KERNEL]->sig_handled();
+}
+
+
+sub shutdown {
+  $_[KERNEL]->call(core => log => 'Shutting down (%s)', $_[ARG1]);
+  $_[KERNEL]->post(pg => 'shutdown');
 }
 
 
