10160385d7c726774b0e1c66f9e06b1f7ba29af2
Yorhel <git@yorhel.nl>
2009-07-17 18:39:23 +0200

    Rewrote Multi::Sitemap and included tag pages
    This should've taken me less than an hour, but unfortunately it took a
    bit longer because I found a rather hard-to-fix bug in PoCo::Pg.
    Be sure to update to the latest POE::Component::Pg.
    
    Also, this change removes the DateTime dependency. Its functionality
    could be replaced with the strftime() found in POSIX (which every Perl
    has)

--- lib/Multi/Sitemap.pm
+++ lib/Multi/Sitemap.pm
@@ -10,17 +10,20 @@ use warnings;
 use POE;
 use XML::Writer;
 use PerlIO::gzip;
-use DateTime;
+use POSIX 'strftime';
+use Time::HiRes 'gettimeofday', 'tv_interval';
 
 
 sub spawn {
   my $p = shift;
   POE::Session->create(
     package_states => [
-      $p => [qw| _start cmd_sitemap staticpages vnpages releasepages producerpages finish addurl |],
+      $p => [qw| _start shutdown check_age generate addquery addurl finish |],
     ],
     heap => {
       output => $VNDB::ROOT.'/www/sitemap.xml.gz',
+      max_age =>  24*3600, # seconds
+      check_delay => 3600, # seconds
       @_,
     }
   );
@@ -29,24 +32,30 @@ sub spawn {
 
 sub _start {
   $_[KERNEL]->alias_set('sitemap');
-  $_[KERNEL]->call(core => register => qr/^sitemap$/, 'cmd_sitemap');
-  
- # Regenerate the sitemap every day on 0:00
-  $_[KERNEL]->post(core => addcron => '0 0 * * *', 'sitemap');
+  $_[KERNEL]->yield('check_age');
+  $_[KERNEL]->sig(shutdown => 'shutdown');
 }
 
 
-sub cmd_sitemap {
-  # Function order:
-  #  cmd_sitemap
-  #  staticpages
-  #  vnpages
-  #  releasepages
-  #  producerpages
-  #  finish
+sub shutdown {
+  $_[KERNEL]->delay('check_age');
+}
 
-  $_[HEAP]{cmd} = $_[ARG0];
+
+sub check_age {
+  # check the last modified time of the sitemap, and if it's older than max_age, regenerate it
+  $_[KERNEL]->yield('generate') if !-f $_[HEAP]{output} || (stat $_[HEAP]{output})[9] < time-$_[HEAP]{max_age};
+
+  # check sitemap again later
+  $_[KERNEL]->delay(check_age => $_[HEAP]{check_delay});
+}
+
+
+sub generate {
+  $_[KERNEL]->call(core => log => '(Re)generating sitemap');
+
   $_[HEAP]{urls} = 0;
+  $_[HEAP]{start} = [ gettimeofday ];
 
   open($_[HEAP]{io}, '>:gzip', $_[HEAP]{output}) || die $1;
   $_[HEAP]{xml} = new XML::Writer(
@@ -56,76 +65,63 @@ sub cmd_sitemap {
     DATA_INDENT => 1
   );
   $_[HEAP]{xml}->xmlDecl();
-  $_[HEAP]{xml}->comment(q|NOTE: All URL's that require you to login or that may contain usernames are left out.|);
   $_[HEAP]{xml}->startTag('urlset', xmlns => 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
-  $_[KERNEL]->yield('staticpages');
-}
-
-
-sub staticpages {
-  $_[KERNEL]->call(core => log => 3, 'Adding static pages');
-
+  # /
   $_[KERNEL]->call(sitemap => addurl => '', 'daily');
 
+  # /d+
   /([0-9]+)$/ && $_[KERNEL]->call(sitemap => addurl => 'd'.$1, 'monthly', [stat $_]->[9])
     for (glob "$VNDB::ROOT/data/docs/*");
 
+  # /v/[browse] & /p/[browse]
   $_[KERNEL]->call(sitemap => addurl => $_, 'weekly')
     for (map { 'v/'.$_, 'p/'.$_ } 'a'..'z', 0, 'all');
 
-  $_[KERNEL]->yield('vnpages');
-}
+  # /v+
+  $_[KERNEL]->post(pg => query => '
+    SELECT v.id, c.added
+      FROM vn v
+      JOIN vn_rev vr ON vr.id = v.latest
+      JOIN changes c ON vr.id = c.id
+      WHERE v.hidden = FALSE
+      ORDER BY v.id',
+    undef, 'addquery', [ 'v', 0.7 ]);
 
-
-sub vnpages {
-  $_[KERNEL]->call(core => log => 3, 'Adding visual novel pages');
-
-  my $q = $Multi::SQL->prepare(q|
-    SELECT v.id, c.added, v.rgraph
-    FROM vn v
-    JOIN vn_rev vr ON vr.id = v.latest
-    JOIN changes c ON vr.id = c.id
-  |);
-  $q->execute;
-  $_[KERNEL]->call(sitemap => addurl => 'v'.$_->[0], 'weekly', $_->[1], 0.7)
-    while(local $_ = $q->fetchrow_arrayref);
-
-  $_[KERNEL]->yield('releasepages');
-}
-
-
-sub releasepages {
-  $_[KERNEL]->call(core => log => 3, 'Adding release pages');
-
-  my $q = $Multi::SQL->prepare(q|
+  # /r+
+  $_[KERNEL]->post(pg => query => '
     SELECT r.id, c.added
-    FROM releases r
-    JOIN releases_rev rr ON rr.id = r.latest
-    JOIN changes c ON c.id = rr.id
-  |);
-  $q->execute;
-  $_[KERNEL]->call(sitemap => addurl => 'r'.$_->[0], 'weekly', $_->[1], 0.3)
-    while(local $_ = $q->fetchrow_arrayref);
+      FROM releases r
+      JOIN releases_rev rr ON rr.id = r.latest
+      JOIN changes c ON c.id = rr.id
+      WHERE r.hidden = FALSE
+      ORDER BY r.id',
+    undef, 'addquery', [ 'r', 0.5 ]);
 
-  $_[KERNEL]->yield('producerpages');
+  # /p+
+  $_[KERNEL]->post(pg => query => '
+    SELECT p.id, c.added
+      FROM producers p
+      JOIN producers_rev pr ON pr.id = p.latest
+      JOIN changes c ON c.id = pr.id
+      WHERE p.hidden = FALSE
+      ORDER BY p.id',
+    undef, 'addquery', [ 'p', 0.3 ]);
+
+  # /g+
+  $_[KERNEL]->post(pg => query => '
+    SELECT t.id, t.added
+      FROM tags t
+      WHERE state = 2
+      ORDER BY t.id',
+    undef, 'addquery', [ 'g', 0.3, 1 ]);
 }
 
 
-sub producerpages {
-  $_[KERNEL]->call(core => log => 3, 'Adding producer pages');
-  
-  my $q = $Multi::SQL->prepare(q|
-    SELECT p.id, c.added
-    FROM producers p
-    JOIN producers_rev pr ON pr.id = p.latest
-    JOIN changes c ON c.id = pr.id
-  |); 
-  $q->execute;
-  $_[KERNEL]->call(sitemap => addurl => 'p'.$_->[0], 'weekly', $_->[1])
-    while(local $_ = $q->fetchrow_arrayref);
-
-  $_[KERNEL]->yield('finish');
+sub addquery { # num, db-res, [ type, priority, finish ]
+  $_[KERNEL]->call(sitemap => addurl => $_[ARG2][0].$_->{id}, 'weekly', $_->{added}, $_[ARG2][1])
+    for(@{$_[ARG1]});
+  $_[KERNEL]->yield('finish') if $_[ARG2][2];
 }
 
 
@@ -133,9 +129,11 @@ sub finish {
   $_[HEAP]{xml}->endTag('urlset');
   $_[HEAP]{xml}->end();
   close $_[HEAP]{io};
-  $_[KERNEL]->call(core => log => 2 => 'Wrote %d URLs in the sitemap', $_[HEAP]{urls});
-  $_[KERNEL]->post(core => finish => $_[HEAP]{cmd});
-  delete @{$_[HEAP]}{qw| xml io cmd urls |};
+
+  $_[KERNEL]->call(core => log => 'Wrote %d URLs (%.1f kB gzipped) to the sitemap in %.2f seconds',
+    $_[HEAP]{urls}, (-s $_[HEAP]{output})/1024, tv_interval($_[HEAP]{start}));
+
+  delete @{$_[HEAP]}{qw| xml io start urls |};
 }
 
 
@@ -143,7 +141,7 @@ sub addurl { # loc, changefreq, lastmod, priority
   $_[HEAP]{xml}->startTag('url');
    $_[HEAP]{xml}->dataElement(loc => $VNDB::S{url}.'/'.$_[ARG0]);
    $_[HEAP]{xml}->dataElement(changefreq => $_[ARG1]) if defined $_[ARG1];
-   $_[HEAP]{xml}->dataElement(lastmod => DateTime->from_epoch(epoch => $_[ARG2])->ymd) if defined $_[ARG2];
+   $_[HEAP]{xml}->dataElement(lastmod => strftime('%Y-%m-%d', gmtime $_[ARG2])) if defined $_[ARG2];
    $_[HEAP]{xml}->dataElement(priority => $_[ARG3]) if defined $_[ARG3];
   $_[HEAP]{xml}->endTag('url');
   $_[HEAP]{urls}++;
