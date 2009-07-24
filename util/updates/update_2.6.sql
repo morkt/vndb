a1fc2a9a103e0f426ac4e496200408a2deccaaf0
3dB <3db@3decibels.net>
2009-07-24 03:40:23 -0400

    Checkin of auth rewrite progress.
    Added a new DB library for handling sessions.
    New update SQL file for database changes.
    Added a line to the global config file to set a global salt. It is separate
    from the cookie_key because it is much more important that it not be changed.

---
+++ util/updates/update_2.6.sql
@@ -0,0 +1,14 @@
+
+-- Create table for session data storage
+
+CREATE TABLE sessions (
+    uid integer NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    token character(40) NOT NULL,
+    expiration bigint DEFAULT 0 NOT NULL,
+    PRIMARY KEY (uid, token)
+);
+
+-- Add column to users for salt storage
+
+ALTER TABLE users ADD COLUMN salt character(9) NOT NULL DEFAULT 0;
+
