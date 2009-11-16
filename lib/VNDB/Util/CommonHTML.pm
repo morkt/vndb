1bddfcb5862be5f40d1a4e5595aa22d79b67b9cd
Yorhel <git@yorhel.nl>
2009-11-16 17:11:41 +0100

    htmlVoteStats(): Fixed another undef in multiplication warning
    I didn't expect c_popularity to be NULL when c_votecount > 0, but forgot that
    this is indeed possible when the votes don't count in the popularity ranking
    (because it's the only or lowest vote of the user).

--- lib/VNDB/Util/CommonHTML.pm
+++ lib/VNDB/Util/CommonHTML.pm
@@ -482,7 +482,7 @@ sub htmlVoteStats {
    if($type eq 'v' && $obj->{c_votecount}) {
      div;
       h3 mt '_votestats_rank_title';
-      p mt '_votestats_rank_pop', $obj->{p_ranking}, sprintf '%.2f', $obj->{c_popularity}*100;
+      p mt '_votestats_rank_pop', $obj->{p_ranking}, sprintf '%.2f', ($obj->{c_popularity}||0)*100;
       p mt '_votestats_rank_rat', $obj->{r_ranking}, sprintf '%.2f', $obj->{c_rating};
      end;
    }
