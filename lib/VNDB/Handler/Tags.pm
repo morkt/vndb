morkt/vndb

commit e69b417f9ce6e73e6c1a477048d167f9490f9d5a

Display VN ratings on tag pages as well
With this method I managed to reuse the VN list table code for the lists
on both the VN browser and the tag pages. And optimized away the
dbTagVNs() function while I was at it (dbVNGet() is powerful enough)

lib/VNDB/Handler/Tags.pm
@@ -29,7 +29,7 @@ sub tagpage {
   return 404 if !$t;
 
   my $f = $self->formValidate(
-    { name => 's', required => 0, default => 'score', enum => [ qw|score title rel pop| ] },
+    { name => 's', required => 0, default => 'tagscore', enum => [ qw|title rel pop tagscore rating| ] },
     { name => 'o', required => 0, default => 'd', enum => [ 'a','d' ] },
     { name => 'p', required => 0, default => 1, template => 'int' },
     { name => 'm', required => 0, default => -1, enum => [qw|0 1 2|] },
@@ -38,12 +38,20 @@ sub tagpage {
   my $tagspoil = $self->reqCookie('tagspoil');
   $f->{m} = $tagspoil =~ /^[0-2]$/ ? $tagspoil : 0 if $f->{m} == -1;
 
-  my($list, $np) = $t->{meta} || $t->{state} != 2 ? ([],0) : $self->dbTagVNs(
-    tag => $tag,
-    order => {score=>'th.rating',title=>'vr.title',rel=>'v.c_released',pop=>'v.c_popularity'}->{$f->{s}}.($f->{o}eq'a'?' ASC':' DESC').($f->{s}eq'score'?', th.users DESC':''),
-    page => $f->{p},
+  my $sortcol = {qw|
+    rel      c_released
+    pop      c_popularity
+    rating   c_rating
+    title    title
+    tagscore tagscore
+  |}->{$f->{s}};
+
+  my($list, $np) = $t->{meta} || $t->{state} != 2 ? ([],0) : $self->dbVNGet(
+    what => 'rating',
     results => 50,
-    maxspoil => $f->{m},
+    page => $f->{p},
+    order => $sortcol.($f->{o} eq 'a' ? ' ASC' : ' DESC'),
+    tags_include => [ $f->{m}, [$tag ]],
   );
 
   my $title = mt '_tagp_title', $t->{meta}?0:1, $t->{name};
@@ -111,7 +119,20 @@ sub tagpage {
   end;
 
   _childtags($self, $t) if @{$t->{childs}};
-  _vnlist($self, $t, $f, $list, $np) if !$t->{meta} && $t->{state} == 2;
+
+  if(!$t->{meta} && $t->{state} == 2) {
+    div class => 'mainbox';
+     h1 mt '_tagp_vnlist';
+     p class => 'browseopts';
+      a href => "/g$t->{id}?m=0", $f->{m} == 0 ? (class => 'optselected') : (), onclick => "setCookie('tagspoil', 0);return true;", mt '_tagp_spoil0';
+      a href => "/g$t->{id}?m=1", $f->{m} == 1 ? (class => 'optselected') : (), onclick => "setCookie('tagspoil', 1);return true;", mt '_tagp_spoil1';
+      a href => "/g$t->{id}?m=2", $f->{m} == 2 ? (class => 'optselected') : (), onclick => "setCookie('tagspoil', 2);return true;", mt '_tagp_spoil2';
+     end;
+     p "\n\n".mt '_tagp_novn' if !@$list;
+     p "\n".mt '_tagp_cached';
+    end;
+    $self->htmlBrowseVN($list, $f, $np, "/g$t->{id}?m=$f->{m}", 1);
+  }
 
   $self->htmlFooter;
 }
@@ -161,63 +182,6 @@ sub _childtags {
    clearfloat;
    br;
   end;
-}
-
-sub _vnlist {
-  my($self, $t, $f, $list, $np) = @_;
-  div class => 'mainbox';
-   h1 mt '_tagp_vnlist';
-   p class => 'browseopts';
-    a href => "/g$t->{id}?m=0", $f->{m} == 0 ? (class => 'optselected') : (), onclick => "setCookie('tagspoil', 0);return true;", mt '_tagp_spoil0';
-    a href => "/g$t->{id}?m=1", $f->{m} == 1 ? (class => 'optselected') : (), onclick => "setCookie('tagspoil', 1);return true;", mt '_tagp_spoil1';
-    a href => "/g$t->{id}?m=2", $f->{m} == 2 ? (class => 'optselected') : (), onclick => "setCookie('tagspoil', 2);return true;", mt '_tagp_spoil2';
-   end;
-   if(!@$list) {
-     p "\n\n".mt '_tagp_novn';
-   }
-   p "\n".mt '_tagp_cached';
-  end;
-  return if !@$list;
-  $self->htmlBrowse(
-    class    => 'tagvnlist',
-    items    => $list,
-    options  => $f,
-    nextpage => $np,
-    pageurl  => "/g$t->{id}?m=$f->{m};o=$f->{o};s=$f->{s}",
-    sorturl  => "/g$t->{id}?m=$f->{m}",
-    header   => [
-      [ mt('_tagp_vncol_score'), 'score' ],
-      [ mt('_tagp_vncol_title'), 'title' ],
-      [ '',                      0       ],
-      [ '',                      0       ],
-      [ mt('_tagp_vncol_rel'),   'rel'   ],
-      [ mt('_tagp_vncol_pop'),   'pop'   ],
-    ],
-    row     => sub {
-      my($s, $n, $l) = @_;
-      Tr $n % 2 ? (class => 'odd') : ();
-       td class => 'tc1';
-        tagscore $l->{rating};
-        i sprintf '(%d)', $l->{users};
-       end;
-       td class => 'tc2';
-        a href => '/v'.$l->{vid}, title => $l->{original}||$l->{title}, shorten $l->{title}, 100;
-       end;
-       td class => 'tc3';
-        $_ ne 'oth' && cssicon $_, mt "_plat_$_"
-          for (sort split /\//, $l->{c_platforms});
-       end;
-       td class => 'tc4';
-        cssicon "lang $_", mt "_lang_$_"
-          for (reverse sort split /\//, $l->{c_languages});
-       end;
-       td class => 'tc5';
-        lit $self->{l10n}->datestr($l->{c_released});
-       end;
-       td class => 'tc6', sprintf '%.2f', ($l->{c_popularity}||0)*100;
-      end;
-    }
-  );
 }
 
 

lib/VNDB/DB/Tags.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use Exporter 'import';
 
-our @EXPORT = qw|dbTagGet dbTagTree dbTagEdit dbTagAdd dbTagMerge dbTagLinks dbTagLinkEdit dbTagStats dbTagVNs|;
+our @EXPORT = qw|dbTagGet dbTagTree dbTagEdit dbTagAdd dbTagMerge dbTagLinks dbTagLinkEdit dbTagStats|;
 
 
 # %options->{ id noid name search state meta page results order what }
@@ -195,32 +195,6 @@ sub dbTagStats {
   return wantarray ? ($r, $np) : $r;
 }
 
-
-# Fetch all VNs from a tag, including VNs from child tags, and provide ratings for them.
-# Argument: %options->{ tag order page results maxspoil }
-sub dbTagVNs {
-  my($self, %o) = @_;
-  $o{order} ||= 'th.rating DESC';
-  $o{page} ||= 1;
-  $o{results} ||= 10;
-
-  my %where = (
-    'tag = ?' => $o{tag},
-    defined $o{maxspoil} ? (
-      'th.spoiler <= ?' => $o{maxspoil} ) : (),
-    'v.hidden = FALSE' => 1,
-  );
-
-  my($r, $np) = $self->dbPage(\%o, q|
-    SELECT th.tag, th.vid, th.users, th.rating, th.spoiler, vr.title, vr.original, v.c_languages, v.c_released, v.c_platforms, v.c_popularity
-      FROM tags_vn_inherit th
-      JOIN vn v ON v.id = th.vid
-      JOIN vn_rev vr ON vr.id = v.latest
-      !W
-      ORDER BY !s|,
-    \%where, $o{order});
-  return wantarray ? ($r, $np) : $r;
-}
 
 1;
 

lib/VNDB/Handler/VNBrowse.pm
@@ -85,52 +85,7 @@ sub list {
   my $url = "/v/$char?q=$f->{q};ti=$f->{ti};te=$f->{te}";
   $_ and $url .= ";pl=$_" for @{$f->{pl}};
   $_ and $url .= ";ln=$_" for @{$f->{ln}};
-  $self->htmlBrowse(
-    class    => 'vnbrowse',
-    items    => $list,
-    options  => $f,
-    nextpage => $np,
-    pageurl  => "$url;o=$f->{o};s=$f->{s}",
-    sorturl  => $url,
-    header   => [
-      @ti ? [ mt('_vnbrowse_col_score'), 'tagscore', undef, 'tc_s' ] : (),
-      [ mt('_vnbrowse_col_title'),       'title', undef, @ti ? 'tc_t' : 'tc1' ],
-      [ '',                              0,       undef, 'tc2' ],
-      [ '',                              0,       undef, 'tc3' ],
-      [ mt('_vnbrowse_col_released'),    'rel',   undef, 'tc4' ],
-      [ mt('_vnbrowse_col_popularity'),  'pop',   undef, 'tc5' ],
-      [ mt('_vnbrowse_col_rating'),      'rating', undef, 'tc6' ],
-    ],
-    row     => sub {
-      my($s, $n, $l) = @_;
-      Tr $n % 2 ? (class => 'odd') : ();
-       if(@ti) {
-         td class => 'tc_s';
-          tagscore $l->{tagscore}, 0;
-         end;
-       }
-       td class => @ti ? 'tc_t' : 'tc1';
-        a href => '/v'.$l->{id}, title => $l->{original}||$l->{title}, shorten $l->{title}, 100;
-       end;
-       td class => 'tc2';
-        $_ ne 'oth' && cssicon $_, mt "_plat_$_"
-          for (sort split /\//, $l->{c_platforms});
-       end;
-       td class => 'tc3';
-        cssicon "lang $_", mt "_lang_$_"
-          for (reverse sort split /\//, $l->{c_languages});
-       end;
-       td class => 'tc4';
-        lit $self->{l10n}->datestr($l->{c_released});
-       end;
-       td class => 'tc5', sprintf '%.2f', ($l->{c_popularity}||0)*100;
-       td class => 'tc6';
-        txt sprintf '%.2f', $l->{c_rating}||0;
-        b class => 'grayedout', sprintf ' (%d)', $l->{c_votecount};
-       end;
-      end;
-    },
-  );
+  $self->htmlBrowseVN($list, $f, $np, $url, scalar @ti);
   $self->htmlFooter;
 }
 

lib/VNDB/Util/CommonHTML.pm
@@ -11,8 +11,8 @@ use Encode 'encode_utf8', 'decode_utf8';
 use POSIX 'ceil';
 
 our @EXPORT = qw|
-  htmlMainTabs htmlDenied htmlHiddenMessage htmlBrowse htmlBrowseNavigate
-  htmlRevision htmlEditMessage htmlItemMessage htmlVoteStats htmlHistory htmlSearchBox htmlRGHeader
+  htmlMainTabs htmlDenied htmlHiddenMessage htmlBrowse htmlBrowseNavigate htmlRevision
+  htmlEditMessage htmlItemMessage htmlVoteStats htmlHistory htmlBrowseVN htmlSearchBox htmlRGHeader
 |;
 
 
@@ -530,6 +530,57 @@ sub htmlHistory {
          end;
         end;
       }
+    },
+  );
+}
+
+
+sub htmlBrowseVN {
+  my($self, $list, $f, $np, $url, $tagscore) = @_;
+  $self->htmlBrowse(
+    class    => 'vnbrowse',
+    items    => $list,
+    options  => $f,
+    nextpage => $np,
+    pageurl  => "$url;o=$f->{o};s=$f->{s}",
+    sorturl  => $url,
+    header   => [
+      $tagscore ? [ mt('_vnbrowse_col_score'), 'tagscore', undef, 'tc_s' ] : (),
+      [ mt('_vnbrowse_col_title'),       'title', undef, $tagscore ? 'tc_t' : 'tc1' ],
+      [ '',                              0,       undef, 'tc2' ],
+      [ '',                              0,       undef, 'tc3' ],
+      [ mt('_vnbrowse_col_released'),    'rel',   undef, 'tc4' ],
+      [ mt('_vnbrowse_col_popularity'),  'pop',   undef, 'tc5' ],
+      [ mt('_vnbrowse_col_rating'),      'rating', undef, 'tc6' ],
+    ],
+    row     => sub {
+      my($s, $n, $l) = @_;
+      Tr $n % 2 ? (class => 'odd') : ();
+       if($tagscore) {
+         td class => 'tc_s';
+          tagscore $l->{tagscore}, 0;
+         end;
+       }
+       td class => $tagscore ? 'tc_t' : 'tc1';
+        a href => '/v'.$l->{id}, title => $l->{original}||$l->{title}, shorten $l->{title}, 100;
+       end;
+       td class => 'tc2';
+        $_ ne 'oth' && cssicon $_, mt "_plat_$_"
+          for (sort split /\//, $l->{c_platforms});
+       end;
+       td class => 'tc3';
+        cssicon "lang $_", mt "_lang_$_"
+          for (reverse sort split /\//, $l->{c_languages});
+       end;
+       td class => 'tc4';
+        lit $self->{l10n}->datestr($l->{c_released});
+       end;
+       td class => 'tc5', sprintf '%.2f', ($l->{c_popularity}||0)*100;
+       td class => 'tc6';
+        txt sprintf '%.2f', $l->{c_rating}||0;
+        b class => 'grayedout', sprintf ' (%d)', $l->{c_votecount};
+       end;
+      end;
     },
   );
 }

commit 975ac4c2eb856b060fae5f6cd014cc338ca2e799

Handler::Tags: Don't display empty VN list
Minor bug introduced in e69b417f

lib/VNDB/Handler/Tags.pm
@@ -122,7 +122,7 @@ sub tagpage {
      p "\n\n".mt '_tagp_novn' if !@$list;
      p "\n".mt '_tagp_cached';
     end;
-    $self->htmlBrowseVN($list, $f, $np, "/g$t->{id}?m=$f->{m}", 1);
+    $self->htmlBrowseVN($list, $f, $np, "/g$t->{id}?m=$f->{m}", 1) if @$list;
   }
 
   $self->htmlFooter;