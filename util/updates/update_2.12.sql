b4b9284ddd4d5e4b1829b87612fbb77e8e6c5d7c
Yorhel <git@yorhel.nl>
2010-10-31 16:15:30 +0100

    Added 1024x576 and 1280x800 screen resolutions

--- util/updates/update_2.12.sql
+++ util/updates/update_2.12.sql
@@ -7,3 +7,8 @@ ALTER TABLE vn ADD COLUMN c_search text;
 CREATE TRIGGER vn_vnsearch_notify         AFTER  UPDATE           ON vn            FOR EACH ROW EXECUTE PROCEDURE vn_vnsearch_notify();
 CREATE TRIGGER vn_vnsearch_notify         AFTER  UPDATE           ON releases      FOR EACH ROW EXECUTE PROCEDURE vn_vnsearch_notify();
 
+
+-- two new resolutions have been added, array indexes have changed
+UPDATE releases_rev SET resolution = resolution + 1 WHERE resolution >= 7;
+UPDATE releases_rev SET resolution = resolution + 1 WHERE resolution >= 11;
+
