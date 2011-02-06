8b3eb97627131a9be85cf784484d8d51d9b417c0
Yorhel <git@yorhel.nl>
2011-02-06 14:12:43 +0100

    Added tag visibility options by category on /v+
    The JS code is a bit messy, though...

--- data/script.js
+++ data/script.js
@@ -474,7 +474,7 @@ function tvsInit() {
   var l = byName(byId('tagops'), 'a');
   for(var i=0;i<l.length; i++)
     l[i].onclick = tvsClick;
-  tvsSet(getCookie('tagspoil'), true);
+  tvsSet(getCookie('tagspoil'), true, (getCookie('tagcat')||'cont,tech').split(','));
 }
 
 function tvsClick() {
@@ -482,29 +482,43 @@ function tvsClick() {
   var l = byName(byId('tagops'), 'a');
   for(var i=0; i<l.length; i++)
     if(l[i] == this) {
-      if(i < 3) {
-        tvsSet(i, null);
-        setCookie('tagspoil', i);
-      } else
-        tvsSet(null, i == 3 ? true : false);
+      if(i < 3) { /* categories */
+        setClass(l[i], 'tsel', !hasClass(l[i], 'tsel'));
+        var c = tvsSet();
+        setCookie('tagcat', c.length ? c.join(',') : '-');
+      } else if(i < 6) { /* spoiler level */
+        tvsSet(i-3, null);
+        setCookie('tagspoil', i-3);
+      } else /* limit */
+        tvsSet(null, i == 6 ? true : false);
     }
   return false;
 }
 
-function tvsSet(lvl, lim) {
+function tvsSet(lvl, lim, cats) {
   /* set/get level and limit to/from the links */
   var l = byName(byId('tagops'), 'a');
+  var cat = cats || [];
   for(var i=0; i<l.length; i++) {
-    if(i < 3) { /* spoiler level */
+    if(i < 3) { /* categories */
+      var c = l[i].href.substr(l[i].href.indexOf('#')+1);
+      if(cats) {
+        for(var j=0; j<cats.length && c != cats[j]; j++) ;
+        setClass(l[i], 'tsel', j != cats.length);
+      } else {
+        if(hasClass(l[i], 'tsel'))
+          cat.push(c);
+      }
+    } else if(i < 6) { /* spoiler level */
       if(lvl != null)
-        setClass(l[i], 'tsel', i == lvl);
+        setClass(l[i], 'tsel', i-3 == lvl);
       if(lvl == null && hasClass(l[i], 'tsel'))
-        lvl = i;
-    } else { /* display limit (3 = summary) */
+        lvl = i-3;
+    } else { /* display limit (6 = summary) */
       if(lim != null)
-        setClass(l[i], 'tsel', lim == (i == 3));
+        setClass(l[i], 'tsel', lim == (i == 6));
       if(lim == null && hasClass(l[i], 'tsel'))
-        lim = i == 3;
+        lim = i == 6;
     }
   }
 
@@ -514,13 +528,14 @@ function tvsSet(lvl, lim) {
   var s=0;
   for(i=0;i<l.length;i++) {
     var thislvl = l[i].className.substr(6, 1);
-    if(thislvl <= lvl && s < lim) {
+    for(var j=0; j<cat.length && !hasClass(l[i], 'cat_'+cat[j]); j++) ;
+    if(thislvl <= lvl && s < lim && j != cat.length) {
       setClass(l[i], 'hidden', false);
       s++;
     } else
       setClass(l[i], 'hidden', true);
   }
-  return false;
+  return cat;
 }
 
 tvsInit();
