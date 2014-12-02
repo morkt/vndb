morkt/vndb

commit 44b11883da3a7e6f1138984ceeba162e0f90dc35

Don't use inline script to pass screenshot sizes in VN edit
Same reasoning as 0d191b5cd8c79eef3cef63c40ded96506a40382a

lib/VNDB/Handler/VNEdit.pm
@@ -298,14 +298,13 @@ sub _form {
     [ static => nolabel => 1, content => mt '_vnedit_scrnorel' ],
   ) : (
     [ hidden => short => 'screenshots' ],
+    [ hidden => short => 'screensizes', value => do {
+      # Current screenshot resolutions, for use by Javascript
+      my @scr = map /^(\d+),/?$1:(), split / /, $frm->{screenshots};
+      my %scr = map +($_->{id}, "$_->{width},$_->{height}"), @{$self->dbScreenshotGet(\@scr)};
+      join ' ', map $scr{$_}, @scr;
+    }],
     [ static => nolabel => 1, content => sub {
-      script type => 'text/javascript';
-       # Screenshot resolutions
-       lit 'VNEDITSCR = {';
-        my @scr = map /^(\d+),/?$1:(), split / /, $frm->{screenshots};
-        lit join ',', map "$_->{id}:[$_->{width},$_->{height}]", @scr ? @{$self->dbScreenshotGet(\@scr)} : ();
-       lit '};';
-      end;
       div class => 'warning';
        lit mt '_vnedit_scrmsg';
       end;

data/script.js
@@ -1008,9 +1008,11 @@ function scrLoad() {
 
   // load the current screenshots
   var scr = byId('screenshots').value.split(' ');
+  var siz = byId('screensizes').value.split(' ');
   for(i=0; i<scr.length && scr[i].length>1; i++) {
     var r = scr[i].split(',');
-    scrSet(scrAdd(r[0], r[1], r[2]), VNEDITSCR[r[0]][0], VNEDITSCR[r[0]][1]);
+    var s = siz[i].split(',');
+    scrSet(scrAdd(r[0], r[1], r[2]), s[0], s[1]);
   }
 
   ivInit();

commit 42e5a4793fc02ef54805ab5706830bbb2e1bd086

Handler::VNEdit: Fix 500 when editing VN without screenshots
Broken in 44b11883da3a7e6f1138984ceeba162e0f90dc35

lib/VNDB/Handler/VNEdit.pm
@@ -301,7 +301,7 @@ sub _form {
     [ hidden => short => 'screensizes', value => do {
       # Current screenshot resolutions, for use by Javascript
       my @scr = map /^(\d+),/?$1:(), split / /, $frm->{screenshots};
-      my %scr = map +($_->{id}, "$_->{width},$_->{height}"), @{$self->dbScreenshotGet(\@scr)};
+      my %scr = map +($_->{id}, "$_->{width},$_->{height}"), @scr ? @{$self->dbScreenshotGet(\@scr)} : ();
       join ' ', map $scr{$_}, @scr;
     }],
     [ static => nolabel => 1, content => sub {