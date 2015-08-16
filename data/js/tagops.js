fcaa479640044cc7430702e5d9c79199d58481d7
Yorhel <git@yorhel.nl>
2015-08-16 11:28:18 +0200

    js: Clean up charops.js & tagops.js
    charops.js was already recently (re)written and followed much of the new
    conventions, so that was easy.
    
    tagops.js has been simplified somewhat, and does not rely on the
    position of the "tagspl#" class anymore.

--- data/js/tagops.js
+++ data/js/tagops.js
@@ -1,68 +1,65 @@
-function tvsInit() {
-  if(!byId('tagops'))
-    return;
-  var l = byName(byId('tagops'), 'a');
-  for(var i=0;i<l.length; i++)
-    l[i].onclick = tvsClick;
-  tvsSet();
+var l, lim, spoil = 0, cats = {};
+
+
+function init() {
+  var i;
+  l = byName(byId('tagops'), 'a');
+
+  // Categories
+  for(i=0; i<3; i++) {
+    l[i].tagops_cat = l[i].href.substr(l[i].href.indexOf('#')+1);
+    l[i].onclick = function() { cats[this.tagops_cat] = !cats[this.tagops_cat]; return set(); };
+    cats[l[i].tagops_cat] = hasClass(l[i], 'tsel');
+  }
+
+  // Spoiler level
+  for(i=3; i<6; i++) {
+    l[i].tagops_spoil = i-3;
+    l[i].onclick = function() { spoil = this.tagops_spoil; return set(); };
+    if(hasClass(l[i], 'tsel'))
+      spoil = i-3;
+  }
+
+  // Summary / all
+  for(i=6; i<8; i++) {
+    l[i].tagops_lim = i == 6;
+    l[i].onclick = function() { lim = this.tagops_lim; return set(); };
+    if(hasClass(l[i], 'tsel'))
+      lim = i == 6;
+  }
+
+  set();
 }
 
-function tvsClick() {
-  var sel;
-  var l = byName(byId('tagops'), 'a');
-  for(var i=0; i<l.length; i++)
-    if(l[i] == this) {
-      if(i < 3) { /* categories */
-        setClass(l[i], 'tsel', !hasClass(l[i], 'tsel'));
-        tvsSet();
-      } else if(i < 6) { /* spoiler level */
-        tvsSet(i-3);
-      } else /* limit */
-        tvsSet(null, i == 6);
-    }
+
+function set() {
+  var i;
+
+  // Set link selection class
+  for(i=0; i<8; i++)
+    setClass(l[i], 'tsel',
+       i < 3 ? cats[l[i].tagops_cat] :
+       i < 6 ? l[i].tagops_spoil == spoil
+             : l[i].tagops_lim == lim);
+
+  // update tag visibility
+  var t = byName(byId('vntags'), 'span');
+  var n = 0;
+  for(i=0; i<t.length; i++) {
+    var v = n < (lim ? 15 : 999);
+    for(var j=0; j<3; j++)
+      if(hasClass(t[i], 'tagspl'+j))
+        v = v && j <= spoil;
+    for(var c in cats)
+      if(hasClass(t[i], 'cat_'+c))
+        v = v && cats[c];
+    setClass(t[i], 'hidden', !v);
+    n += v?1:0;
+  }
+
   return false;
 }
 
-function tvsSet(lvl, lim, cats) {
-  /* set/get level and limit to/from the links */
-  var l = byName(byId('tagops'), 'a');
-  var cat = cats || [];
-  for(var i=0; i<l.length; i++) {
-    if(i < 3) { /* categories */
-      var c = l[i].href.substr(l[i].href.indexOf('#')+1);
-      if(cats) {
-        for(var j=0; j<cats.length && c != cats[j]; j++) ;
-        setClass(l[i], 'tsel', j != cats.length);
-      } else {
-        if(hasClass(l[i], 'tsel'))
-          cat.push(c);
-      }
-    } else if(i < 6) { /* spoiler level */
-      if(lvl != null)
-        setClass(l[i], 'tsel', i-3 == lvl);
-      if(lvl == null && hasClass(l[i], 'tsel'))
-        lvl = i-3;
-    } else { /* display limit (6 = summary) */
-      if(lim != null)
-        setClass(l[i], 'tsel', lim == (i == 6));
-      if(lim == null && hasClass(l[i], 'tsel'))
-        lim = i == 6;
-    }
-  }
 
-  /* update tag visibility */
-  l = byName(byId('vntags'), 'span');
-  lim = lim ? 15 : 999;
-  var s=0;
-  for(i=0;i<l.length;i++) {
-    var thislvl = l[i].className.substr(6, 1);
-    for(var j=0; j<cat.length && !hasClass(l[i], 'cat_'+cat[j]); j++) ;
-    if(thislvl <= lvl && s < lim && j != cat.length) {
-      setClass(l[i], 'hidden', false);
-      s++;
-    } else
-      setClass(l[i], 'hidden', true);
-  }
-}
-
-tvsInit();
+if(byId('tagops'))
+  init();
